9a6a0952362165fd7ec66ed93f657b0ff5c6d312
Viviana161296 <viviana161296@gmail.com>
2019-09-12 10:58:30 -0500

    View

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,8 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
+
+<h1>holaaa</h1>
         <div id="app">
             <todo-component></todo-component>
         </div>
